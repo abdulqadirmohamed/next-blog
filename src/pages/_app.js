0e951aabc97af23d6a17b2a99524944a30fdae0d
abdulqadir <cajiibxaaji02@gmail.com>
2023-05-20 22:35:14 +0300

    deployment ready

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,11 @@
-import { Header } from '@/components/header'
+import { Header, Nav } from '@/components/Nav'
 import '@/styles/globals.css'
 import '@/styles/style.css'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-    <Header/>
+    <Nav/>
     <div className='w-[90%] mx-auto'>
     <Component {...pageProps} />
 
--- src/pages/test.jsx
+++ src/pages/test.jsx
@@ -1,80 +1,94 @@
-import React from "react";
-import Image from "next/image";
-import Img1 from "../img/img1.jpg";
-import Img2 from "../img/img2.jpg";
-import Img3 from "../img/img3.jpg";
-import Img4 from "../img/img4.jpg";
-import Img5 from "../img/img5.jpg";
-const test = () => {
-  const africaNews = [
-    {
-      id: 1,
-      cover: Img5,
-      title: "Exploring the rigging claims in Nigeria's elections",
-      slug: "Exploring-the-rigging",
-      description:
-        " How credible are the opposition claims of fraud in Nigeria's presidential election?",
-        tags: 'africa'
-    },
-    {
-      id: 2,
-      cover: Img2,
-      title: "Africa Live: Obi plans to challenge Tinubu's Nigeria victory",
-      slug: "Africa-live",
-      description:
-        "The Labour Party presidential candidate finished third in the poll - and more stories.",
-        tags: 'science'
-    },
-    {
-      id: 3,
-      cover: Img3,
-      title: "Africa Live: Obi plans to challenge Tinubu's Nigeria victory",
-      slug: "Africa-live",
-      description:
-        "The Labour Party presidential candidate finished third in the poll - and more stories.",
-        tags: 'Europe'
-    },
-    {
-      id: 3,
-      cover: Img4,
-      title: "Africa Live: Obi plans to challenge Tinubu's Nigeria victory",
-      slug: "Africa-live",
-      description:
-        "The Labour Party presidential candidate finished third in the poll - and more stories.",
-        tags: 'Lating America'
-    },
-    {
-      id: 3,
-      cover: Img1,
-      title: "Africa Live: Obi plans to challenge Tinubu's Nigeria victory",
-      slug: "Africa-live",
-      description:
-        "The Labour Party presidential candidate finished third in the poll - and more stories.",
-        tags: 'africa'
-    },
-  ]
-    // console.log(items)
+import { useState } from "react";
+import { useRouter } from "next/router";
+
+const images = [
+  "https://images.pexels.com/photos/1179234/pexels-photo-1179234.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/371570/pexels-photo-371570.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/1027393/pexels-photo-1027393.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+];
+
+// const slider = [
+//   {
+//     id:1,
+//     cover: "https://images.pexels.com/photos/1179234/pexels-photo-1179234.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+//     title: 'Wi-Fi like speeds, now on the go.',
+//     description: 'With Airtel 5G Plus. Up to 30x faster',
+//     link: 'Order Now'
+//   },
+//   {
+//     id:2,
+//     cover : "https://images.pexels.com/photos/371570/pexels-photo-371570.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+//     title: 'Family of 2. Or Family of 4.',
+//     description: 'Get Airtel Postpaid Family Plan for your special family',
+//     link: 'Order Now'
+//   }
+// ]
+export default function test() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+
+
+  const previousImage = () => {
+    setCurrentIndex(currentIndex === 0 ? images.length - 1 : currentIndex - 1);
+  };
+
+  const nextImage = () => {
+    setCurrentIndex(currentIndex === images.length - 1 ? 0 : currentIndex + 1);
+  };
+  const router = useRouter();
+
   return (
-    <div className="wrapper  h-[400px]">
-    {africaNews.map((news, index) => (
-      <div className="grid md:grid-col-2 relative hover:opacity-90 cursor-pointer">
-        <Image src={news.cover} alt="image" fill />
-        <div className="absolute bottom-0 p-3 text-white bg-gradient-to-t from-black ">
-          <h1 className="text-[30px] font-semibold leading-7 mb-2 line-clamp-2">
-            Greek transport minister resigns after deadly train crash
-          </h1>
-          <p className="leading-6 line-clamp-2 pragraph_hidden">
-            The prime minister of Greece, Kyriakos Mitsotakis, has begun an
-            address to the nation about the train crash.
-          </p>
-          <span className="pl-3 border-l-2 border-green-600 my-4 uppercase text-[12px] text-gray-200">
-            {news.tags}
-          </span>
+    <div className="relative">
+      <img
+        src={images[currentIndex]}
+        alt="carousel image"
+        className="w-full h-[400px] object-cover"
+      />
+      <div className="absolute left-2 bottom-6">
+        <div className="flex gap-6 items-center">
+          <button
+            onClick={previousImage}
+            className="bg-black rounded-full text-white p-2"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-3 h-3"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 19.5L8.25 12l7.5-7.5"
+              />
+            </svg>
+          </button>
+          <div className="text-white">
+            {currentIndex + 1} / {images.length}
+          </div>
+          <button
+            onClick={nextImage}
+            className="bg-black rounded-full text-white p-2"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-3 h-3"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M8.25 4.5l7.5 7.5-7.5 7.5"
+              />
+            </svg>
+          </button>
         </div>
       </div>
-    ))}
-  </div>
+    </div>
   );
-};
-
-export default test;
+}
